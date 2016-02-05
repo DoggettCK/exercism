d735044cb7db365c3794646167c716a96fad25d7
Chris Doggett <CDoggett@covermymeds.com>
2016-02-05 10:17:17 -0600

    Finish word-count in Elixir

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,11 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-
+    sentence
+    |> String.split(~r/[!@%$^&* _,:]+/, trim: true)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reduce(%{}, &increment_word_count/2)
   end
+
+  defp increment_word_count(word, acc), do: Dict.update(acc, word, 1, &(&1 + 1))
 end
