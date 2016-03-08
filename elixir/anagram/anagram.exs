e0e5c2e7dcbe7487b80906149a169c8a7db7ba05
Chris Doggett <CDoggett@covermymeds.com>
2016-03-08 13:57:49 -0600

    Finish anagram in Elixir

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,34 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    match(base, candidates, [])
+  end
 
+  defp match(_, [], results), do: results |> Enum.reverse
+  defp match(base, [head | tail], results) do
+    match(base, tail, anagram?(base, head, results))
+  end
+
+  defp anagram?(base, head, results) do
+    clean_base = base |> clean
+    clean_head = head |> clean
+
+    cond do
+      clean_base === clean_head -> results 
+      (clean_base |> sort) === (clean_head |> sort) -> [head | results]
+      true -> results
+    end
+  end
+
+  defp clean(string) when is_binary(string) do
+    string
+    |> String.downcase
+  end
+
+  defp sort(string) when is_binary(string) do
+    string
+    |> to_char_list
+    |> Enum.sort
+    |> to_string
   end
 end
