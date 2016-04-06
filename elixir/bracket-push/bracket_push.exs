ffdcea170332e8f9a8935c25eb3d40158b3ff05f
Chris Doggett <CDoggett@covermymeds.com>
2016-04-06 14:07:12 -0500

    Cleaned up bracket-push with comprehension

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -9,14 +9,10 @@ defmodule BracketPush do
 
   defp check_brackets(<<>>, []), do: true
   defp check_brackets(<<>>, _), do: false
-  defp check_brackets(<<"[", rest::binary>>, history), do: check_brackets(rest, ["[" | history])
-  defp check_brackets(<<"]", rest::binary>>, ["[" | history]), do: check_brackets(rest, history)
-  defp check_brackets(<<"]", _::binary>>, [_ | _history]), do: false
-  defp check_brackets(<<"{", rest::binary>>, history), do: check_brackets(rest, ["{" | history])
-  defp check_brackets(<<"}", rest::binary>>, ["{" | history]), do: check_brackets(rest, history)
-  defp check_brackets(<<"}", _::binary>>, [_ | _history]), do: false
-  defp check_brackets(<<"(", rest::binary>>, history), do: check_brackets(rest, ["(" | history])
-  defp check_brackets(<<")", rest::binary>>, ["(" | history]), do: check_brackets(rest, history)
-  defp check_brackets(<<")", _::binary>>, [_ | _history]), do: false
+  for {open, close} <- [{"{", "}"}, {"[", "]"}, {"(", ")"}] do
+    defp check_brackets(unquote(open) <> rest, history), do: check_brackets(rest, [unquote(open) | history])
+    defp check_brackets(unquote(close) <> rest, [unquote(open) | history]), do: check_brackets(rest, history)
+    defp check_brackets(unquote(close) <> _, [_ | _history]), do: false
+  end
   defp check_brackets(<<_::binary-size(1), rest::binary>>, history), do: check_brackets(rest, history)
 end
