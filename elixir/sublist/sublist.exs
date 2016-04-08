0b296563e3fde6b3e0517f6073e3fb934d3264ba
Chris Doggett <CDoggett@covermymeds.com>
2016-04-08 17:00:38 -0500

    Removed final conds from Elixir exercises

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,14 +3,16 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
-    cond do
-      equal?(a, b) -> :equal
-      contains?(b, a) -> :sublist
-      contains?(a, b) -> :superlist
-      true -> :unequal
-    end
-  end
+  def compare(a, b), do: check_equal(a, b, equal?(a, b))
+
+  defp check_equal(_, _, true), do: :equal
+  defp check_equal(a, b, _), do: check_sublist(a, b, contains?(b, a))
+
+  defp check_sublist(_, _, true), do: :sublist
+  defp check_sublist(a, b, _), do: check_superlist(a, b, contains?(a, b))
+
+  defp check_superlist(_, _, true), do: :superlist
+  defp check_superlist(_, _, _), do: :unequal
 
   defp equal?([], []), do: true
   defp equal?([h | t1], [h | t2]), do: equal?(t1, t2)
@@ -19,12 +21,7 @@ defmodule Sublist do
   defp contains?(_, []), do: true
   defp contains?([], _), do: false
   defp contains?([h | t1] = one, [h | _] = two) do
-    cond do
-      one
-      |> Enum.take(Enum.count(two))
-      |> equal?(two) -> true
-      true -> contains?(t1, two)
-    end
+    Enum.take(one, Enum.count(two)) |> equal?(two) or contains?(t1, two)
   end
   defp contains?([_ | t1], l2), do: contains?(t1, l2)
 end
