556c9c4d2d53cd6044b4b28a57847f699181907a
Chris Doggett <CDoggett@covermymeds.com>
2016-05-03 10:38:44 -0500

    Moved to clearer regex solution

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -3,18 +3,10 @@ defmodule Acronym do
   Generate an acronym from a string. 
   "This is a string" => "TIAS"
   """
-  @spec abbreviate(string) :: String.t()
+  @spec abbreviate(string :: String.t()) :: String.t()
   def abbreviate(string) do
-    string
-    |> String.split(~r{[^\w]}, trim: true)
-    |> Enum.flat_map(&split_camel_case/1)
-    |> Enum.map(&String.first/1)
+    Regex.scan(~r/\b\w|\p{Lu}/, string)
     |> Enum.join
     |> String.upcase
-  end
-
-  defp split_camel_case(string) do
-    string
-    |> String.split(~r{(?<!^)(?=[A-Z])}, trim: true)
   end
 end
