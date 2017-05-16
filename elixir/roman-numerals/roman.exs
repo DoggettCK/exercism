3dc56fe62643945dfa8d97acec1ca6c0bc004eee
Chris Doggett <CDoggett@covermymeds.com>
2017-05-16 10:41:29 -0500

    Switched to keyword list

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -7,22 +7,10 @@ defmodule Roman do
     numerals(number, "")
   end
 
-  for {arabic, roman} <- [
-    {1000, "M"},
-    {900, "CM"},
-    {500, "D"},
-    {400, "CD"},
-    {100, "C"},
-    {90, "XC"},
-    {50, "L"},
-    {40, "XL"},
-    {10, "X"},
-    {9, "IX"},
-    {5, "V"},
-    {4, "IV"},
-    {1, "I"}
-  ] do
-    defp numerals(number, result) when number >= unquote(arabic), do: numerals(number - unquote(arabic), result <> unquote(roman))
+  @conversions [M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1]
+
+  for {roman, arabic} <- @conversions do
+    defp numerals(number, result) when number >= unquote(arabic), do: numerals(number - unquote(arabic), result <> unquote(roman |> to_string))
   end
 
   defp numerals(_, result), do: result
